f852cad72d7442beea4220a6c8ed8e6ffb4cc3b4
Johanhzzz <joh.hernandezc@duocuc.cl>
2025-07-02 00:53:21 -0400

    Se agrego texto a carrito y se exporto, solo para funionamiento de la  pagina

---
+++ trasherbox/src/pages/Carrito.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Carrito = () => {
+  return (
+    <div>
+      <h1>Carrito</h1>
+      <p>Tu carrito está vacío.</p>
+    </div>
+  );
+};
+
+export default Carrito;
